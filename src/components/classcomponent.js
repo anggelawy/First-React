a60249ac1d6b3a9b95a6bf8877443b3f025f3b0f
Anggela Widyehadi <masteranggel248@gmail.com>
2023-08-24 13:33:59 +0700

    first commit

---
+++ src/components/classcomponent.js
@@ -0,0 +1,10 @@
+import React from "react"
+class Classcomponent extends React.Component{
+    render() {
+        return (
+            <div><h1>kode ini termasuk class component</h1></div>
+        )
+    }
+}
+
+export default Classcomponent 
---
+++ src/components/introduction.js
@@ -0,0 +1,12 @@
+function Introduction(props) {
+    // console.log('parameter utama : ', props.name) untuk test console di google   
+  return (
+    <div>
+      <h1>Sesi 1</h1>
+      <p> Nama saya adalah : {props.name}</p>     
+    </div>
+
+  );
+}
+
+export default Introduction;
